11762d57248481639636dd279c227d3905ad0374
Mikalai Lazitski <mlazitski@elinext.com>
2022-11-24 17:47:07 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,21 @@ import {
     getDirectoryData,
     convertObject,
     getFlattenArray,
+    getLocalGraphData,
+    constructGraphData
 } from "../lib/utils";
 import FolderTree from "../components/FolderTree";
+import dynamic from 'next/dynamic'
 import MDContent from "../components/MDContent";
 
 
-export default function Home({content, tree, flattenNodes, backLinks}) {
+// This trick is to dynamically load component that interact with window object (browser only)
+const DynamicGraph = dynamic(
+    () => import('../components/Graph'),
+    { loading: () => <p>Loading ...</p>, ssr: false }
+)
+
+export default function Home({graphData, content, tree, flattenNodes, backLinks}) {
     return (
         <Layout>
             <div className = 'container'>
@@ -17,8 +26,30 @@ export default function Home({content, tree, flattenNodes, backLinks}) {
                     <FolderTree tree={tree} flattenNodes={flattenNodes}/>
                 </nav>
                 <MDContent content={content}  handleOpenNewContent={null} backLinks={backLinks}/>
+                <DynamicGraph graph={graphData}/>
             </div>
         </Layout>
     );
 
+}
+const {nodes, edges} = constructGraphData()
+
+export function getStaticProps() {
+    const tree = convertObject(getDirectoryData());
+    const contentData = getSinglePost("index");
+    const flattenNodes = getFlattenArray(tree)
+    const listOfEdges =   edges.filter(anEdge => anEdge.target === "index")
+    const internalLinks = listOfEdges.map(anEdge => nodes.find(aNode => aNode.slug === anEdge.source)).filter(element => element !== undefined)
+    const backLinks = [...new Set(internalLinks)]
+
+    const graphData = getLocalGraphData("index");
+    return {
+        props: {
+            content: contentData.data,
+            tree: tree,
+            flattenNodes: flattenNodes,
+            graphData:graphData,
+            backLinks: backLinks
+        },
+    };
 }
